3c96ed6f42bd500fcc6cc2b49f28bb556c98d5d1
Ikey Doherty <ikey.doherty@gmail.com>
2014-04-18 20:30:36 -0700

    sidebar-window: Update documentation to be useful

--- src/sidebar-window.h
+++ src/sidebar-window.h
@@ -41,6 +41,29 @@ typedef struct _GatSidebarWindowPriv GatSidebarWindowPrivate;
 #define GAT_IS_SIDEBAR_WINDOW_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), GAT_SIDEBAR_WINDOW_TYPE))
 #define GAT_SIDEBAR_WINDOW_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), GAT_SIDEBAR_WINDOW_TYPE, GatSidebarWindowClass))
 
+/**
+ * SECTION:sidebar-window
+ * @short_description: A window with an automatic sidebar
+ * @title: GatSidebarWindow
+ *
+ * A GatSidebarWindow enables you to quickly build an application UI
+ * that contains a content area, and a sidebar area.
+ *
+ * The application window is automatically split into two major parts,
+ * and enables separate "headers" (titles) for the sidebar area, and
+ * the main content area.
+ *
+ * When adding content to the window, you should use #gat_sidebar_window_get_content_area
+ * This returns a #GtkBox in a vertical orientation, enabling you to add
+ * anything you wish.
+ *
+ * When manipulating the sidebar area, you should use #gat_sidebar_window_get_sidebar
+ * This returns a #GtkRevealer which has its content hidden by default.
+ * This enables derived UIs to hide the sidebar when it is not required.
+ * Note no transition is enabled by default, this is left to the application
+ * developer to implement.
+ */
+
 /* GatSidebarWindow object */
 struct _GatSidebarWindow {
         GatGnomeAppWindow parent;
@@ -66,7 +89,15 @@ GtkWidget *gat_sidebar_window_new(void);
 /**
  * gat_sidebar_window_get_sidebar:
  * @window: A GatSidebarWindow
- * 
+ *
+ * This method returns the sidebar area of the #GatSidebarWindow. By
+ * default this is a #GtkRevealer, which is hidden. When adding to
+ * the sidebar area, do so as with any normal #GtkContainer.
+ *
+ * This uses the left-hand side of the UI, and will only use as much
+ * space as required by the widget. Notice that the sidebar header
+ * will match the size of the sidebar itself.
+ *
  * Returns: (transfer none): the sidebar
  */
 GtkWidget *gat_sidebar_window_get_sidebar(GatSidebarWindow *window);
@@ -74,6 +105,10 @@ GtkWidget *gat_sidebar_window_get_sidebar(GatSidebarWindow *window);
 /**
  * gat_sidebar_window_get_content_area:
  * @window: A GatSidebarWindow
+ *
+ * This method returns the main content area of the #GatSidebarWindow
+ * This is by default a vertical #GtkBox, which is configured to use
+ * as much space as possible within the UI.
  * 
  * Returns: (transfer none): content area
  */
@@ -84,7 +119,9 @@ GtkWidget *gat_sidebar_window_get_content_area(GatSidebarWindow *window);
  * @window: A GatSidebarWindow
  * @title: The new sidebar title
  *
- * Update the sidebar title
+ * Update the sidebar title of the #GatSidebarWindow. This will be displayed
+ * in the header area above the sidebar itself. The UI will automatically
+ * adjust the allocation using a #GtkSizeGroup
  */
 void gat_sidebar_window_set_sidebar_title(GatSidebarWindow *window, const gchar *title);
 
@@ -92,7 +129,8 @@ void gat_sidebar_window_set_sidebar_title(GatSidebarWindow *window, const gchar 
  * gat_sidebar_window_get_sidebar_title:
  * @window: A GatSidebarWindow
  * 
- * Get the current sidebar title
+ * Get the currently set sidebar title. Do not free this string, as
+ * #GatSidebarWindow owns this reference.
  *
  * Returns: (transfer none): Current sidebar title
  */

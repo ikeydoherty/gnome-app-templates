8f8823e1845a6ffaa9936989be2ffbc68c03372a
Ikey Doherty <ikey.doherty@gmail.com>
2014-04-21 17:52:54 -0700

    sidebar: Update documentation

--- src/gat-sidebar.h
+++ src/gat-sidebar.h
@@ -39,6 +39,32 @@ typedef struct _GatSidebarPriv GatSidebarPrivate;
 #define GAT_IS_SIDEBAR_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), GAT_SIDEBAR_TYPE))
 #define GAT_SIDEBAR_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), GAT_SIDEBAR_TYPE, GatSidebarClass))
 
+/**
+ * SECTION:gat-sidebar
+ * @short_description: An automatic sidebar widget
+ * @title: GatSidebar
+ *
+ * A GatSidebarWindow enables you to quickly and easily provide a consistent
+ * "sidebar" object for your user interface.
+ *
+ * In order to use a GatSidebar, you simply use a GtkStack to organise
+ * your UI flow, and add the sidebar to your sidebar area. You can use
+ * #gat_sidebar_set_stack to connect the #GatSidebar to the #GtkStack
+ *
+ * The GatSidebar will obtain the titles from the "title" child-property
+ * of the item within the GtkStack. Theming is automatically handled within
+ * the library to ensure a consistent look across applications. Note that
+ * users may use the touch, key or mouse events to interact with this
+ * widget and select an item within your #GtkStack instance.
+ *
+ * It should be of interest to developers that a #GatSidebar subclasses
+ * #GtkScrolledWindow - this means you can enable the scrolled window
+ * policy for automatic scrollbars, etc, within a #GatSidebar. Note that
+ * scrollbars are disabled by default, leaving it up to the application
+ * developer to decide on their UI appearance.
+ *
+ */
+
 /* GatSidebar object */
 struct _GatSidebar {
         GtkScrolledWindow parent;
@@ -68,7 +94,9 @@ GtkWidget *gat_sidebar_new(void);
  * @sidebar: A GatSidebar
  * @stack: a GtkStack
  *
- * Set the stack associated with this GatSidebar
+ * Set the #GtkStack associated with this #GatSidebar. This sidebar widget
+ * will automatically update according to the order (packing) and items
+ * within the given #GtkStack.
  */
 void gat_sidebar_set_stack(GatSidebar *sidebar, GtkStack *stack);
 
